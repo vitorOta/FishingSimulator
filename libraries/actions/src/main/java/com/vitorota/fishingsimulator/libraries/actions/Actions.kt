827e4144363dfe40f1cccffab6e38980e375e2fa
vitorOta <ota.vitor@gmail.com>
2019-05-20 10:19:18 -0300

    adjust in  openLoginIntent

--- libraries/actions/src/main/java/com/vitorota/fishingsimulator/libraries/actions/Actions.kt
+++ libraries/actions/src/main/java/com/vitorota/fishingsimulator/libraries/actions/Actions.kt
@@ -13,7 +13,9 @@ import android.content.Intent
 object Actions {
     private const val prefix = "com.vitorota.fishingsimulator.features"
 
-    fun openLoginIntent(context: Context) = internalIntent(context, "$prefix.login.open").apply { flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK}
+    fun openLoginIntent(context: Context) = internalIntent(context, "$prefix.login.open").apply {
+        flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NO_ANIMATION
+    }
 
     fun internalIntent(context: Context, action: String) = Intent(action).setPackage(context.packageName)
 }
